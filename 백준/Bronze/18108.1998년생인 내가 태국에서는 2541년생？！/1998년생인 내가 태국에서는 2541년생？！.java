5e4c4948073181470f19756c14fea9e0593e3ddf
leedayeong <77225709+leedayeong@users.noreply.github.com>
2022-01-28 00:16:47 +0900

    [Bronze V] Title: 1998년생인 내가 태국에서는 2541년생?!, Time: 132 ms, Memory: 14200 KB -BaekjoonHub

---
+++ 백준/Bronze/18108.1998년생인 내가 태국에서는 2541년생？！/1998년생인 내가 태국에서는 2541년생？！.java
@@ -0,0 +1,15 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int year = Integer.parseInt(br.readLine());
+		System.out.println(year-543);
+	}
+
+}
