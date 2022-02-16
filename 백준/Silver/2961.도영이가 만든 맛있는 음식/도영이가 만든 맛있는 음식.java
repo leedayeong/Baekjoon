e296baa53278457268adf2f35b2cb55d13f61b53
leedayeong <77225709+leedayeong@users.noreply.github.com>
2022-02-16 14:02:44 +0900

    [Silver I] Title: 도영이가 만든 맛있는 음식, Time: 120 ms, Memory: 14204 KB -BaekjoonHub

--- 백준/Silver/2961.도영이가 만든 맛있는 음식/도영이가 만든 맛있는 음식.java
+++ 백준/Silver/2961.도영이가 만든 맛있는 음식/도영이가 만든 맛있는 음식.java
@@ -5,6 +5,7 @@ import java.util.StringTokenizer;
 public class Main {
 	static int N, min;
 	static int[][] src;
+//	static boolean[] select;
 	
 	public static void main(String[] args) throws Exception{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -12,6 +13,7 @@ public class Main {
 		
 		//초기화
 		src = new int[N][2];
+//		select = new boolean[N];
 		
 		for (int i = 0; i < N; i++) {
 			StringTokenizer st = new StringTokenizer(br.readLine());
@@ -21,22 +23,29 @@ public class Main {
 		
 		min = Integer.MAX_VALUE;
 		
-		comb(0, 1, 0); // 신 맛은 곱하기때문에 1을 줌 ! 
+		// 모든 부분집합의 수
+		int size = 1 << src.length; // 5개 기준 (2*2*2*2*2) : 000001 => 100000 이 됨 
+		
+		for (int i = 1; i < size; i++) { // 모든 경우의 수를 도는 것임 !  i range 0 ~ 31 
+										 // 하지만, 문제에서는  공집합을 제외하라고했음(재료는 한개이상)! 그래서 i = 1 부터 시작하도록 !
+			// 한 개의 부분집합을 의미
+			
+			int sin = 1;
+			int ssn = 0;
+			
+			for (int j = 0; j < src.length ; j++) { // 한 개의 만들어진 부분집합을 한자리씩 도는 것 ! (선택 유무 확인 용도)
+				if( (i & (1 << j)) != 0 ) { // i가 select의 의미인데 만약 i=0 => 000000 , i=1 => 000001, i=2 -> 0000010 ..., i = 31 -> 011111
+											// j 가 인덱스 0, 1, 2, 3, 4 번째 자리 탐색했을 때 i가 1인 경우 => 선택됨! 
+											// 즉, 위의 i가 표현하는 부분집합 0과 1중 현재 j가 선택되어 있다는 의미
+					sin *= src[j][0];
+					ssn += src[j][1];
+				}
+			}
+			
+			min = Math.min(min, Math.abs(sin - ssn));
+			
+		}
 		
 		System.out.println(min);
-	}
-	
-	static void comb(int srcIdx, int sinSum, int ssnSum) {
-		
-		if(srcIdx == N) return; // 모든 재료를 다 확인해봤으면 return 하는 것 ! 
-		
-		int currSin = src[srcIdx][0] * sinSum;
-		int currSsn = src[srcIdx][1] + ssnSum;
-		
-		min = Math.min(min, Math.abs(currSin - currSsn));
-	
-		comb(srcIdx + 1, currSin, currSsn); // 현재의 선택 O !
-		comb(srcIdx + 1, sinSum, ssnSum);	// 현재 선택  X => 파라미터로 받은 값 그대로 전달 !  
-		
 	}
 }
