0da511cea417ad62d7062532fd89e08f27a67914
leedayeong <77225709+leedayeong@users.noreply.github.com>
2022-04-05 20:39:09 +0900

    [Silver I] Title: 회전 초밥, Time: 152 ms, Memory: 16356 KB -BaekjoonHub

--- 백준/Silver/2531. 회전 초밥/회전 초밥.java
+++ 백준/Silver/2531. 회전 초밥/회전 초밥.java
@@ -1,43 +1,70 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
 import java.util.StringTokenizer;
 
 public class Main {
-	static int N, d, k, c, ans;
-	static List<Integer> dish;
 	
+	static int N, d, k, c, ans;
+	static int[] visit;
+	static int[] food;
+	static int[] first;
 	public static void main(String[] args) throws Exception{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
+		
 		N = Integer.parseInt(st.nextToken());
 		d = Integer.parseInt(st.nextToken());
 		k = Integer.parseInt(st.nextToken());
 		c = Integer.parseInt(st.nextToken());
 		
-		dish = new ArrayList<Integer>();
+		visit = new int[d+1];
+		food = new int[k];
+		first = new int[k];
+		int cnt = 0;
 		
-		for (int i = 0; i < N; i++) {
-			dish.add(Integer.parseInt(br.readLine()));
+		//쿠폰 추가
+		visit[c]++;
+		cnt++;
+		
+		for (int i = 0; i < k; i++) {
+			food[i] = Integer.parseInt(br.readLine());
+			first[i] = food[i];
+			visit[food[i]] ++;
+			if(visit[food[i]] == 1) cnt++;
 		}
 		
-		for (int i = N; i < N+k-1; i++) {
-			dish.add(dish.get(i%N));
+		ans = cnt;
+		
+		int idx = 0;
+		for (int i = k; i < N; i++) {
+			int num = food[idx];
+			if(visit[num] == 1) cnt--; // 종류 줄어듦
+			visit[num] --;
+			
+			food[idx] = Integer.parseInt(br.readLine());
+			if(visit[food[idx]] == 0) cnt++; // 종류 추가
+			visit[food[idx]] ++;
+			
+			idx = (idx+1) == k ? 0 : idx+1;
+			
+			ans = Math.max(ans, cnt);
 		}
 		
-		ans = Integer.MIN_VALUE;
-		for (int i = 0; i < N; i++) {
-			Set<Integer> kindOfDish = new HashSet<>( dish.subList(i, i+k) );
-			int size = kindOfDish.contains(c) ? kindOfDish.size() : kindOfDish.size()+1;
-			if(size == d) {
-				System.out.println(d);
-				return;
-			}
-			ans = Math.max(ans, size);
+		// 다 돌고 뒤쪽에서 다시 앞접시! 
+		for (int i = 0; i < k; i++) {
+			int num = food[idx];
+			if(visit[num] == 1) cnt--; // 종류 줄어듦
+			visit[num] --;
+			
+			food[idx] = first[i];
+			if(visit[food[idx]] == 0) cnt++; // 종류 추가
+			visit[food[idx]] ++;
+			
+			idx = (idx+1) == k ? 0 : idx+1;
+			
+			ans = Math.max(ans, cnt);
 		}
+		
 		System.out.println(ans);
 	}
 }
