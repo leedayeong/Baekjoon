b8ca77556425e609adc2292ab27bda590e8e53b5
leedayeong <77225709+leedayeong@users.noreply.github.com>
2022-07-26 21:02:04 +0900

    [Gold III] Title: 크게 만들기, Time: 264 ms, Memory: 33948 KB -BaekjoonHub

---
+++ 백준/Gold/2812. 크게 만들기/크게 만들기.java
@@ -0,0 +1,43 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.StringTokenizer;
+
+public class Main {
+	
+	static int N, K;
+	static String num;
+	static Deque<Integer>dq;
+	static int cnt;
+	
+	public static void main(String[] args) throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		StringBuilder sb = new StringBuilder();
+		
+		N = Integer.parseInt(st.nextToken());
+		K = Integer.parseInt(st.nextToken());
+		
+		dq = new ArrayDeque<>();
+		
+		num = br.readLine();
+		
+		for (int i = 0; i < N; i++) {
+			int n = num.charAt(i) - '0';
+			
+			while(cnt < K && !dq.isEmpty() && dq.getLast() < n) {
+				dq.pollLast();
+				cnt++;
+			}
+			
+			dq.addLast(n);
+		}
+		
+		for (int i = 0; i < N-K; i++) {
+			sb.append(dq.pollFirst());
+		}
+		
+		System.out.println(sb);
+	}
+}
